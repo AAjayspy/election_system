66ea6919fae8f05016205c789a35d01216d3a49e
Ajay Arsud <ajay.arsud@gmail.com>
2021-04-18 23:37:48 +0530

    Added devise default landing page sidekiq redis server.

--- app/controllers/dashboard_controller.rb
+++
@@ -1,5 +0,0 @@
-class DashboardController < ApplicationController
-  def index
-    @message = "static page"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  root to: "dashboard#index"
+  # get 'home/index'
+  # devise_for :accounts
+  # root to: "dashboard#index"
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :accounts, controllers: {
+    sessions: 'accounts/sessions',
+    registrations: 'accounts/registrations',
+    omniauth_callbacks: 'accounts/omni_auth',
+    passwords: 'accounts/passwords',
+    confirmations: 'accounts/confirmations',
+    unlocks: 'accounts/unlocks'
+
+  }
+  # devise_scope :accounts do
+  #   get 'sign_in', to: 'accounts/sessions#new'
+  #   get 'sign_up', to: 'accounts/registrations#new'
+  #   get 'forgot_password', to: 'accounts/passwords#new'
+  #   get 'reset_password', to: 'accounts/passwords#edit'
+  # end
 end
